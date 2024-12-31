34007a2ce895c85d76fecdedacdc480b3a75f0af
味噌味噌 <92548681+misosyokunin@users.noreply.github.com>
2025-01-01 02:45:00 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,10 @@
 "use strict";
 alert(1);
+
+function getJSON(link){
+  const response = await fetch(link);
+	return response.json();
+}
+
+const aho = await getJSON("./test.json");
+console.log(aho);
